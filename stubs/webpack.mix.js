3e2907421e55ec00f43aa1f1ca84f15aa2984fb5
Richard Le Poidevin <ric@sirric.co.uk>
2022-06-09 10:36:07 +0100

    Update webpack.mix.js
    Removed purge CSS
    Added Favicon settings

--- stubs/webpack.mix.js
+++ stubs/webpack.mix.js
@@ -1,16 +1,9 @@
 const mix = require('laravel-mix');
-let glob = require("glob-all");
-let PurgecssPlugin = require("purgecss-webpack-plugin");
+let glob = require('glob-all');
 let path = require('path');
 require('laravel-mix-bundle-analyzer');
 require('laravel-mix-favicon');
 require('laravel-mix-stylelint');
-
-class PurgeCssExtractor {
-	static extract(content) {
-		return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
-	}
-}
 
 mix.js('resources/js/app.js', 'public/js')
 	.vue()
@@ -36,40 +29,12 @@ mix.browserSync({
 // and so you still have the full set of utilities available during development.
 if (mix.inProduction()) {
 	mix.version();
-	mix.favicon();
-	mix.bundleAnalyzer();
-
-	/*mix.webpackConfig({
-		plugins: [
-			new PurgecssPlugin({
-
-				// Specify the locations of any files you want to scan for class names.
-				paths: glob.sync([
-					path.join(__dirname, "public/*.php"),
-					path.join(__dirname, "resources/views/**/*.blade.php"),
-					path.join(__dirname, "resources/js/**/*.vue")
-				]),
-				extractors: [
-					{
-						extractor: PurgeCssExtractor,
-
-						// Specify the file extensions to include when scanning for
-						// class names.
-						extensions: ["html", "js", "php", "vue"]
-					}
-				],
-				whitelistPatternsChildren: []
-			})
-		]
-<<<<<<< HEAD
+	mix.favicon({
+		blade: 'resources/views/partials/_favicon.blade.php',
 	});
-
-=======
-	});*/
-
->>>>>>> 12c98fd6d9387546b1f40b7e19a20c24d5ec0866
-	let date = new Date();
-	console.log('Release version: ' + date.getFullYear() + '-' + pad(date.getMonth()) + '-' + pad(date.getDay()) + '-' + pad(date.getUTCHours()) + '-' + pad(date.getUTCMinutes()));
+	mix.bundleAnalyzer();
 }
+
+
 
 function pad(n){return n<10 ? '0'+n : n}
